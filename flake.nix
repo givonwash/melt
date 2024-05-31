bc06565b5fac32c8d4bb627053133a1a2de3d36e
givonwash <givonwash@gmail.com>
2024-05-31 14:44:56 -0700

    chore(devShell): set up `pre-commit` hooks

--- flake.nix
+++ flake.nix
@@ -3,17 +3,23 @@
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
+    git-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nixpkgs.url = "github:NixOs/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = { self, flake-utils, nixpkgs }:
+  outputs = { self, flake-utils, git-hooks, nixpkgs }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
       in
       {
         devShells.default = pkgs.mkShell {
+          inherit (self.checks.${system}.gitHooks) shellHook;
           buildInputs = with pkgs; [
+            self.checks.${system}.gitHooks.enabledPackages
             kubectl
             kubernetes-helm
             minikube
@@ -22,6 +28,16 @@
             postgresql
             python312
           ];
+        };
+
+        checks = {
+          gitHooks = git-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              end-of-file-fixer.enable = true;
+              nixpkgs-fmt.enable = true;
+            };
+          };
         };
       });
 }
