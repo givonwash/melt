ad34ff62024417a227e4f728d79b6ca3918550c4
Givon Washington <46825663+givonwash@users.noreply.github.com>
2024-06-27 23:46:21 -0700

    fix(dbt): force depedency between the `purchases` & `products` models (#13)

--- melt-dbt/models/marts/shop/purchases.sql
+++ melt-dbt/models/marts/shop/purchases.sql
@@ -1,3 +1,9 @@
+-- fmt: off
+-- NOTE: this model depends on the `products` model as `purchases.product_id` is a foreign key for
+--       `products.product_id`
+-- fmt: on
+--
+-- depends_on: {{ ref('products') }}
 with final as (select * from {{ ref("int_faker__purchases_joined_with_products") }})
 
 select *
